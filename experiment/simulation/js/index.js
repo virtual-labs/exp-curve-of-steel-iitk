e8cb998e5f65219dd141a209559607fc9852548a
vlabiitk <vlabiitk@gmail.com>
2024-08-07 16:47:02 +0530

    update simulator

--- experiment/simulation/js/index.js
+++ experiment/simulation/js/index.js
@@ -307,7 +307,7 @@ function handleStep5() {
       {
         "page": 1,
         "title": "What does the acronym UTM stand for?",
-        "options": ["Universal Tensile Machine", "Uniaxial Testing Machine", "Universal Testing Machine", "Uniaxial Tensile Machine"],
+        "options": ["Universal Tensile Machine", "Uniaxial Testing Machine", "Universal Testing Machine", "Ultimate Tensile Machine"],
         "correct": 2
       },
       {
@@ -361,9 +361,9 @@ function handleStep5() {
       {
         "page": 10,
         "title": "Which of the following points are not observed during tensile testing of medium carbon steel?",
-        "options": ["Upper yield point", "Lower yield point", "Failure", "cold drawing"],
+        "options": ["Upper yield point", "Lower yield point", "Failure", "Cold drawing"],
         "correct": 2
-      },
+    },
       {
         "page": 11,
         "title": "Engineering strain is given as 0.2, then the true strain at that point is:",
